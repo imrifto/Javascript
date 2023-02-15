da31567489c04886c694133f53c878311ab2fc7b
imrifto <rifto.ang@gmail.com>
2023-02-15 09:37:16 +0600

    adding shorthand

---
+++ shorthand.js
@@ -0,0 +1,18 @@
+// If you want to change the variable valu here is the short Hand :
+// var x = 10;
+
+// update valu :
+// x = 20;
+
+// Add oparation :
+// x = x + 10;
+
+// you add oparation shorly :
+// x += 10;
+// x -= 10;
+// x *= 10;
+// x /= 10;
+
+// Vary short Hand :
+// x++; (Adding +1)
+// x--; (Removing -1)
