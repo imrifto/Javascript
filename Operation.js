501d2e6d733380cd440324dc254eb7a77b4d6c21
imrifto <rifto.ang@gmail.com>
2023-02-15 09:18:32 +0600

    first commit

---
+++ Operation.js
@@ -0,0 +1,9 @@
+// There are Many Tipes of Math operations :
+// 1. Addition         +
+// 2. Substraction     -
+// 3. Multipication    *
+// 4. Divition         /    (vagfol)
+// 5. Remainder        %    (vagsas)
+
+// -------------------------------------
+// you can say x + y : here (x,y) are operand and (+) is operatior
---
+++ var.js
@@ -0,0 +1,20 @@
+// Var is Called as Variable.
+// It's like the (div).
+// you can put manny info in var !
+// var x = 10;
+// var y = 10;
+// total = x + y;
+// console.log(total);
+
+// There are 3 types of Variables :
+// 1. Numbaric         =   12
+// 2. String           =   'I am string'
+// 3. Bollian          =   true/false
+
+// Variable Name :
+// 1. CamelNortation   =   var imCamel = 20;
+
+// You can't seet keyword as Variable Name ! 
+// var true    = 'This is Wrong';
+// var false   = 'This is Wrong';
+// var return  = 'This is Wrong';
