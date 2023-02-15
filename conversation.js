85db946b25ee9bdd16421f4f062019cc3fd1fb82
imrifto <rifto.ang@gmail.com>
2023-02-15 10:09:51 +0600

    Adding parseint ,parseFloat,conversation

---
+++ conversation.js
@@ -0,0 +1,20 @@
+// There are many ways to make the total result
+
+//Numbaric :
+// var price1 = 10;
+// var price2 = 10;
+// totalPrice = price1 + price2; (Numbaric just work as math)
+
+// String :
+// var firstName = 'Abdullah';
+// var lastName  = 'Al-Miraz';
+// fullName = firstName + lastName ; 
+// (String just add the last thing result will be 'AbdullahAl-Miraz')
+
+//Add Space In string :
+// var firstName = 'Abdullah';
+// var lastName  = 'Al-Miraz';
+// fullName = firstName + ' ' + lastName ; (Result Will Be 'Abdullah Al-Miraz') 
+
+// --------------  You Cant use numberic as string (x = '10' y ='10') result = 1010
+// --------------  You Cant use single thing as numberic as string (x = 10 y ='10') result = 1010
---
+++ parseInt.js
@@ -0,0 +1,17 @@
+// you can convert any string valu as numberic parseInt (Parse Intiger) :
+// parsInt (PornoSonkha):
+
+// var x = '10'; (This valu is numbaric but i used string so the out put will be string )
+//               (But if i used ParseInt it will convert to numbaric)
+
+// Example :
+
+// var x = '10';
+// convert = parseInt(x)
+// console.log(convert);
+
+// Example :
+// var x = '5.00';
+// convert = parseInt(x)
+// console.log(convert); (result will be just '5' beacuse it parsInt is pornosonkha !)
+parseFloat
