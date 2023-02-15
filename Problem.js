720ce528d856c8e79f5cb567a0d55a2ba0272fcc
imrifto <rifto.ang@gmail.com>
2023-02-15 12:05:09 +0600

    adding problems

---
+++ Problem.js
@@ -0,0 +1,28 @@
+// Problem 1 :
+// var harrMoney   = 1000;
+// var appleOrange = 700;
+// shopkipperGive  = harrMoney - appleOrange;
+// console.log(shopkipperGive);
+
+// Problem 2 :
+// var banglaMarks  = 75.25;
+// var englishMarks = 65;
+// var mathMarks    = 80;
+// var phicMarks    = 35.45;
+// var bioMark      = 99.50;
+// totalMarks       = banglaMarks + englishMarks + mathMarks + phicMarks + bioMark;
+// avrMarks         = totalMarks/5;
+// avrMarks         = avrMarks.toFixed(2)
+// console.log(avrMarks);
+
+//Problem 3 :
+// var firstLine   = 'I am going to be'
+// var secoundLine = 'an awsome web-devoloper'
+// total = firstLine + ' ' + secoundLine;
+// console.log(total);
+
+//Problem 4 :
+// var saraMomNumer  = 119;
+// var dividedNumber = 5;
+// sum               = saraMomNumer % dividedNumber;
+// console.log(sum);
