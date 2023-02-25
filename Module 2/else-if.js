4d96387208f41ddb47d015c24eabfe75faa4566d
imrifto <rifto.ang@gmail.com>
2023-02-25 09:29:48 +0600

    adding more

---
+++ Module 2/else-if.js
@@ -0,0 +1,17 @@
+// else if : using multiple rules :
+var myMoney = 400;
+var iphone  = 100000;
+var k20Pro  = 20000;
+var nokia   = 1000;
+if(iphone < myMoney){
+    console.log('Horray I am rich');
+}
+else if(k20Pro < myMoney){
+    console.log('beginning of my succses !');
+}
+else if(nokia < myMoney){
+    console.log('i am really poor');
+}
+else{
+    console.log('i am notihing but a pice of shit , why i am so lazy.. go and work harder');
+}
---
+++ Module 2/if-else.js
@@ -0,0 +1,9 @@
+// if and else :
+var iphonePrice = 250000;
+var myBudget    = 400;
+if (iphonePrice <= myBudget) {
+    console.log('shutup and give me the phone');    
+}
+else{
+    console.log('I need to work really hard beacuse i am poor');
+}
