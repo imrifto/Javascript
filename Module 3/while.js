9e9b384ae563bf11bd3061a16b5daf3ea1e0d311
imrifto <rifto.ang@gmail.com>
2023-02-25 10:20:41 +0600

    say hello to loop

---
+++ Module 3/while.js
@@ -0,0 +1,10 @@
+// While is used to loop somthing (you can control or make it infinait) :
+var roastGiven = 0;
+
+// aikhana roastGiven er thaka 7 true (boro) ai jonno loop hocha
+while(roastGiven < 7){
+    console.log('Aga Amara Roast Dan ! ');
+    // roastGiven er valu +1 korcha 
+    console.log(roastGiven);
+    roastGiven++;   
+}
